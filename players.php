3e3404ff0509065fd0720b039c4a72b282ffd939
Jacob Nash <jpnash@mail.bradley.edu>
2015-06-09 15:54:56 -0500

    Changed Framework of Player BIO
    These changes streamline the page and allow for tabbed segments

--- players.php
+++ players.php
@@ -22,70 +22,83 @@
 	</div>
 	<div class="main">
 		<div class="space">
-			<div class="segment">
+			<div class="basesegment">
 				<div class="space">
 					<h1>
 					Roll Fizzlebeef
 					</h1>
 				</div>
-
-				<div class="leftmedia">
-					<div class="space">
-						<img class="players" src="images/hitter.jpg">
+				<div class="space">
+					<div class="profilepicture">
+						<div class="space">
+							<img class="players" src="images/hitter.jpg">
+						</div>
 					</div>
-				</div>
 
-				<div class="mediachoice">
-					<div class="space">
-						Here would be additional choices for media
+					<div class="mediachoice">
+						<div class="space">
+							Here would be additional choices for media
+						</div>
 					</div>
-				</div>
+					
+					<div class="basestatistics">
+						<div class="space">
+							<p>
+								Here is where the stats will be
+							</p>
+						</div>
+					</div>
 
-				<div class="rightmedia">
-					<div class="space">
-						<img class="vid" src="images/4by3.jpg">
+					
+				</div>
+			</div>
+			<div class="space">
+				<div class="ssegment">
+					<div class="fourtab">
+						<div class="sspace">
+							Tab
+						</div>
+					</div>
+					<div class="fourtab">
+						<div class="sspace">
+							Tab
+						</div>
+					</div>
+					<div class="fourtab">
+						<div class="sspace">
+							Tab
+						</div>
+					</div>
+					<div class="fourtab">
+						<div class="sspace">
+							Tab
+						</div>
 					</div>
 				</div>
 			</div>
-			<div class="segment">
-				<div class="leftbox">
-					<div class="sspace">
-						<h1 class="light">
-						General Attributes
-						</h1>
-						<p>
-							Name:			Roll Fizzlebeef<br><br>
-							Height:			5ft 10in<br><br>
-							Weight:			140 lbs<br><br>
-							Date of Birth:	January 1st, 1999<br><br>
-							School:			Richwoods<br><br>
-							City:			Peoria<br><br>
-							State:			Illinois<br><br>
-							Current Team:	RHS Knights<br><br>
-						</p>
+			<div class="tabbedsegment">
+				<div class="space">
+					<div class="ssegment">
+						<div class="threetab">
+							<div class="sspace">
+								Tab
+							</div>
+						</div>
+						<div class="threetab">
+							<div class="sspace">
+								Tab
+							</div>
+						</div>
+						<div class="threetab">
+							<div class="sspace">
+								Tab
+							</div>
+						</div>
 					</div>
 				</div>
-				<div class="rightbox">
-					<div class="sspace">
-						<h1 class="light">
-						Playing Statistics
-						</h1>
-						<p>
-							Position:				<br><br>
-							Bat Speed:				<br><br>
-							Arm Speed:				<br><br>
-							60 Yard Dash:			<br><br>
-							40 Yard Dash:			<br><br>
-							Bat Average:			<br><br>
-							Runs Scored:			<br><br>
-							Stolen Bases:			<br><br>
-						</p>
-					</div>
+				<div class="space">
+					Here is the video and stats
 				</div>
-
-
-
-
 			</div>
 		</div>
 	</div>
