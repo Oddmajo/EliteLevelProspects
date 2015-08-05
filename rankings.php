c62a82cbfdeb032e87defc2f55e7a195b36441d5
Joe Sorgea <joe.sorgea@gmail.com>
2015-08-05 17:24:11 -0500

    Graphs
    There's now an initial graph implementation in rankings.php. It doesn't
    use the database yet but that's mostly because there's not enough data.

--- rankings.php
+++ rankings.php
@@ -16,8 +16,6 @@
     <!-- Custom styles for this template -->
     <!-- <link href="commonstyles.css" rel="stylesheet" type="text/css"> -->
 
-    <script src="bootstrap/assets/js/ie-emulation-modes-warning.js"></script>
-
     <style>
         .sorted-column {
             color: #029EDC;
@@ -43,6 +41,11 @@
         }
     ?>
     <div class="container">
+        <h2>Progression</h2>
+        <canvas id="myChart" width="800" height="400"></canvas>
+        <h2>Five-Tool Model</h2>
+        <canvas id="myRadarChart" width="400" height="400"></canvas>
+
         <div class="alert alert-info">
           <strong>Tip:</strong> To sort the table by a column, click the column title you want to sort by.
         </div>
@@ -64,18 +67,17 @@
             <tbody>
                 <?php
                     // query the player details and bat speed tables and select the latest bat speed entry for each player, then order the table by bat speed
-                    $query = "SELECT first_name, last_name, bat_speed, exit_velocity, bench_press, dead_lift, squat, ranking, player_details.player_id, batspeed_stats.player_id, player_strength.player_id, MAX(batspeed_stats.date_stats_collected)
+                    $query = "SELECT first_name, last_name, bat_speed, exit_velocity, bench_press, dead_lift, squat, player_details.player_id, batspeed_stats.player_id, player_strength.player_id, MAX(batspeed_stats.date_stats_collected)
                     FROM player_details, batspeed_stats, player_strength
                     WHERE player_details.player_id = batspeed_stats.player_id AND player_details.player_id = player_strength.player_id
-                    GROUP BY player_details.player_id
-                    ORDER BY ranking DESC";
+                    GROUP BY player_details.player_id";
                     $result = $conn->query($query);
 
                     while($row = $result->fetch_assoc()) {
                         ?>
                         <tr>
                             <td><?php echo $row["first_name"] . " " . $row["last_name"]?></td>
-                            <td><?php echo $row["ranking"]?></td>
+                            <td>N/A</td>
                             <td><?php echo $row["bat_speed"]?> mph</td>
                             <td><?php echo $row["exit_velocity"]?> mph</td>
                             <td><?php echo $row["bench_press"]?> lb</td>
@@ -172,18 +174,17 @@
             <tbody>
                 <?php
                     // query the player details and bat speed tables and select the latest bat speed entry for each player, then order the table by bat speed
-                    $query = "SELECT first_name, last_name, sixty_yard_time, vertical_leap, shuttle_run, reach, ranking, player_details.player_id, infielder_stats.player_id, MAX(date_stats_collected)
+                    $query = "SELECT first_name, last_name, sixty_yard_time, vertical_leap, shuttle_run, reach, player_details.player_id, infielder_stats.player_id, MAX(date_stats_collected)
                     FROM player_details, infielder_stats
                     WHERE player_details.player_id = infielder_stats.player_id
-                    GROUP BY player_details.player_id
-                    ORDER BY ranking DESC";
+                    GROUP BY player_details.player_id";
                     $result = $conn->query($query);
 
                     while($row = $result->fetch_assoc()) {
                         ?>
                         <tr>
                             <td><?php echo $row["first_name"] . " " . $row["last_name"]?></td>
-                            <td><?php echo $row["ranking"]?></td>
+                            <td>N/A</td>
                             <td><?php echo $row["sixty_yard_time"]?> s</td>
                             <td>N/A</td>
                             <td><?php echo $row["vertical_leap"]?> in</td>
@@ -203,14 +204,154 @@
     <script src="bootstrap/js/bootstrap.min.js"></script>
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
     <script src="bootstrap/assets/js/ie10-viewport-bug-workaround.js"></script>
-
+    <script src="bootstrap/assets/js/ie-emulation-modes-warning.js"></script>
+    <!-- Custom js libraries -->
     <script type="text/javascript" src="tablesorter/js/jquery.tablesorter.js"></script>
+    <script src="Chart.js/Chart.js"></script>
 
     <script>
         $(document).ready(function(){
             $(function(){
                 $("table").tablesorter();
 
+                var ctx = $("#myChart").get(0).getContext("2d");
+                var data = {
+                    labels: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
+                    datasets: [
+                        {
+                            label: "My First dataset",
+                            fillColor: "rgba(2, 158, 220, 0.2)",
+                            strokeColor: "rgba(2, 158, 220, 1)",
+                            pointColor: "rgba(2, 158, 220, 1)",
+                            pointStrokeColor: "#fff",
+                            pointHighlightFill: "#fff",
+                            pointHighlightStroke: "rgba(220,220,220,1)",
+                            data: [30, 32, 33, 35, 37, 39, 40, 45, 49, 55, 62, 71]
+                        }
+                    ]
+                };
+                var options = {
+                    ///Boolean - Whether grid lines are shown across the chart
+                    scaleShowGridLines : true,
+
+                    //String - Colour of the grid lines
+                    scaleGridLineColor : "rgba(0,0,0,.05)",
+
+                    //Number - Width of the grid lines
+                    scaleGridLineWidth : 1,
+
+                    //Boolean - Whether to show horizontal lines (except X axis)
+                    scaleShowHorizontalLines: true,
+
+                    //Boolean - Whether to show vertical lines (except Y axis)
+                    scaleShowVerticalLines: true,
+
+                    //Boolean - Whether the line is curved between points
+                    bezierCurve : true,
+
+                    //Number - Tension of the bezier curve between points
+                    bezierCurveTension : 0.4,
+
+                    //Boolean - Whether to show a dot for each point
+                    pointDot : true,
+
+                    //Number - Radius of each point dot in pixels
+                    pointDotRadius : 4,
+
+                    //Number - Pixel width of point dot stroke
+                    pointDotStrokeWidth : 1,
+
+                    //Number - amount extra to add to the radius to cater for hit detection outside the drawn point
+                    pointHitDetectionRadius : 20,
+
+                    //Boolean - Whether to show a stroke for datasets
+                    datasetStroke : true,
+
+                    //Number - Pixel width of dataset stroke
+                    datasetStrokeWidth : 2,
+
+                    //Boolean - Whether to fill the dataset with a colour
+                    datasetFill : true,
+
+                    //String - A legend template
+                    legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].strokeColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>"
+
+                };
+                var myLineChart = new Chart(ctx).Line(data, options);
+
+                ctx = $("#myRadarChart").get(0).getContext("2d");
+                data = {
+                    labels: ["Speed", "Arm Strength", "Hitting for Average", "Hitting for Power", "Fielding"],
+                    datasets: [
+                        {
+                            label: "My First dataset",
+                            fillColor: "rgba(2, 158, 220, 0.2)",
+                            strokeColor: "rgba(2, 158, 220, 1)",
+                            pointColor: "rgba(2, 158, 220, 1)",
+                            pointStrokeColor: "#fff",
+                            pointHighlightFill: "#fff",
+                            pointHighlightStroke: "rgba(220,220,220,1)",
+                            data: [0.85, 0.9, 0.7, 0.6, 0.4]
+                        }
+                    ]
+                }
+                options = {
+                    //Boolean - Whether to show lines for each scale point
+                    scaleShowLine : true,
+
+                    //Boolean - Whether we show the angle lines out of the radar
+                    angleShowLineOut : true,
+
+                    //Boolean - Whether to show labels on the scale
+                    scaleShowLabels : false,
+
+                    // Boolean - Whether the scale should begin at zero
+                    scaleBeginAtZero : true,
+
+                    //String - Colour of the angle line
+                    angleLineColor : "rgba(0,0,0,.1)",
+
+                    //Number - Pixel width of the angle line
+                    angleLineWidth : 1,
+
+                    //String - Point label font declaration
+                    pointLabelFontFamily : "'Arial'",
+
+                    //String - Point label font weight
+                    pointLabelFontStyle : "normal",
+
+                    //Number - Point label font size in pixels
+                    pointLabelFontSize : 10,
+
+                    //String - Point label font colour
+                    pointLabelFontColor : "#666",
+
+                    //Boolean - Whether to show a dot for each point
+                    pointDot : true,
+
+                    //Number - Radius of each point dot in pixels
+                    pointDotRadius : 3,
+
+                    //Number - Pixel width of point dot stroke
+                    pointDotStrokeWidth : 1,
+
+                    //Number - amount extra to add to the radius to cater for hit detection outside the drawn point
+                    pointHitDetectionRadius : 20,
+
+                    //Boolean - Whether to show a stroke for datasets
+                    datasetStroke : true,
+
+                    //Number - Pixel width of dataset stroke
+                    datasetStrokeWidth : 2,
+
+                    //Boolean - Whether to fill the dataset with a colour
+                    datasetFill : true,
+
+                    //String - A legend template
+                    legendTemplate : "<ul class=\"<%=name.toLowerCase()%>-legend\"><% for (var i=0; i<datasets.length; i++){%><li><span style=\"background-color:<%=datasets[i].strokeColor%>\"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>"
+
+                }
+                var myRadarChart = new Chart(ctx).Radar(data, options);
                 // bind to sort events
                 $("table")
                 .bind("sortStart",function(e, table) {
